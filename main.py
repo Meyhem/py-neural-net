48e74ab10284079c4bcdcbad41ad7604e55ef517
meyhem <meyhem@protonmail.com>
2020-07-09 21:39:58 +0200

    Replaced Logistic with relu, training until maximal allowed SSE

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as p
 
 
-def logistic_function(x: np.ndarray) -> np.ndarray:
-    return 1/(1 + np.exp(-x))
+def relu(x: np.ndarray) -> np.ndarray:
+    return np.maximum(0, x)
 
 
-# simplified derivation according to https://towardsdatascience.com/derivative-of-the-sigmoid-function-536880cf918e
-def dx_logistic_function(x: np.ndarray) -> np.ndarray:
-    return x*(1 - x)
+def dx_relu(x: np.ndarray) -> np.ndarray:
+    return 1. * (x > 0)
 
 
 class Layer:
@@ -17,7 +15,7 @@ class Layer:
         self.biases: np.ndarray = np.random.random(size=cols) - 0.5
 
     def forward(self, input_vector: np.ndarray) -> np.ndarray:
-        return logistic_function(input_vector.dot(self.weights) + self.biases)
+        return relu(input_vector.dot(self.weights) + self.biases)
 
     def __str__(self):
         return "weights={}\nbiases={}".format(self.weights, self.biases)
@@ -43,12 +41,12 @@ def backward_pass(net: [Layer], y: np.ndarray, activations: [np.ndarray]) -> [np
 
         if layer == net[-1]:
             errors.append(y - activations[-1])
-            delta = errors[-1] * dx_logistic_function(activations[-1])
+            delta = errors[-1] * dx_relu(activations[-1])
             deltas.append(delta)
         else:
             next_layer = net[i + 1]
             errors.append(np.dot(next_layer.weights, deltas[-1].T))
-            delta = errors[-1].T * dx_logistic_function(activations[i])
+            delta = errors[-1].T * dx_relu(activations[i])
             deltas.append(delta)
 
     return list(reversed(deltas)), list(reversed(errors))
@@ -60,27 +58,31 @@ def apply_deltas(net: [Layer], deltas: [np.ndarray], errors: [np.ndarray], lr: f
         l.biases += errors[i] * lr
 
 
-def train(net: [Layer], x: np.ndarray, y: np.ndarray):
-    for xi, yi in zip(x, y):
-        activations = forward_pass(net, xi)
-        deltas, errors = backward_pass(net, yi, activations)
-        apply_deltas(net, deltas, errors, 0.3)
+def train(net: [Layer], x: np.ndarray, y: np.ndarray, lr: float):
+    # run each input 100 times, MLP requires fewer epochs to learn that way
+    for _ in range(100):
+        for xi, yi in zip(x, y):
+            activations = forward_pass(net, xi)
+            deltas, errors = backward_pass(net, yi, activations)
+            apply_deltas(net, deltas, errors, lr)
 
 
 X = np.asarray([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y = np.asarray([[0], [0], [0], [1]])
 nn: [Layer] = [Layer(2, 3), Layer(3, 3), Layer(3, 1)]
 
-for i in range(50000):
-    train(nn, X, Y)
+epochs = 0
+while True:
+    train(nn, X, Y, 0.05)
+    out = forward_pass(nn, X)[-1]
+    err = np.sum(np.square(Y - out))
 
-    if i % 1000 == 0:
-        out = forward_pass(nn, X)[-1]
-        print(Y - out)
-        err = np.sum(np.square(Y - out))
-        print(err)
+    if err < 0.01:
+        print('Trained in {} epochs. SSE={}'.format(epochs, err))
+        break
+    epochs += 1
 
-print('predict', forward_pass(nn, np.asarray([1, 0]))[-1])
-# print(forward_pass(nn, np.array([1,1])))
-
-p.show()
+print('predict 0 & 0', forward_pass(nn, np.asarray([0, 0]))[-1])
+print('predict 1 & 0', forward_pass(nn, np.asarray([1, 0]))[-1])
+print('predict 0 & 1', forward_pass(nn, np.asarray([0, 1]))[-1])
+print('predict 1 & 1', forward_pass(nn, np.asarray([1, 1]))[-1])
